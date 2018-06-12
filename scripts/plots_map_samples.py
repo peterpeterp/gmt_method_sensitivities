a62e77ba772677bd8473b2ce6b1d4319c8e73b99
Peter <peterpfleiderer@gmx.de>
2018-06-12 17:22:08 +0200

    only relevant plots

--- scripts/plots_map_samples.py
+++ scripts/plots_map_samples.py
@@ -48,47 +48,6 @@ arguments={
 }
 pl_mp.plot_maps(**arguments)
 
-# CanESM2 tas
-arguments={
-	'files':['data_models/CanESM2_r1i1p1/sample_tas.nc','data_models/CanESM2_r1i1p1/sample_tas.nc'],
-	'var_names':['tas','tas'],
-	'titles':['January','August'],
-	'time_steps':[0,7],
-	'outfile':'gmt_method_sensitivities/figures/maps_CanESM2_tas.png',
-	'label':'tas [K]',
-	'color_range':[270,300],
-	'extend':[-10,20,30,65],
-	'nrows':1,
-}
-pl_mp.plot_maps(**arguments)
-
-# CanESM2 tos
-arguments={
-	'files':['data_models/CanESM2_r1i1p1/sample_tos.nc','data_models/CanESM2_r1i1p1/sample_tos.nc'],
-	'var_names':['tos','tos'],
-	'titles':['January','August'],
-	'time_steps':[0,7],
-	'outfile':'gmt_method_sensitivities/figures/maps_CanESM2_tos.png',
-	'label':'tos [K]',
-	'color_range':[270,300],
-	'extend':[-10,20,30,65],
-	'nrows':1,
-}
-pl_mp.plot_maps(**arguments)
-
-# CanESM2 sftof replace
-arguments={
-	'files':['sftof_regrid/ACCESS1-0_remapdis.nc','sftof_regrid/CanESM2_remapdis.nc'],
-	'var_names':['sftof','sftof'],
-	'titles':['ACCESS1-0','CanESM2'],
-	'outfile':'gmt_method_sensitivities/figures/maps_sftof_CanESM2_ACCESS1-0.png',
-	'label':'sftof [0-100]',
-	'color_range':[0,100],
-	'extend':[-10,20,30,65],
-	'nrows':1,
-}
-pl_mp.plot_maps(**arguments)
-
 # IPSL-CM5A-LR sftof replace
 arguments={
 	'files':['sftof_regrid/IPSL-CM5A-LR_remapbil.nc','sftof_regrid/IPSL-CM5A-LR_remapbil_NanTreated.nc','sftof_regrid/ACCESS1-0_remapbil.nc'],
